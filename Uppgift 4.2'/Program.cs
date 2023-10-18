d4b94b1aa0d0d3520830ccc8311a9a2dcf233763
Admin <Admin@TEB123018>
2023-10-18 10:33:01 +0200

    Add project files.

---
+++ Uppgift 4.2'/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Uppgift_4._1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool avsluta = false;
+        int störstHeltal = 0;
+
+
+        while (avsluta != true) {
+            Console.WriteLine("Vill du skriva ett heltal? Om inte, skriv 'n', annars skriv ditt heltal");
+            string svar = Console.ReadLine();
+
+            try
+            {
+                if (svar == "n")
+                {
+                    avsluta = true;
+                }
+                else if (störstHeltal < Convert.ToInt32(svar))
+                {
+                    störstHeltal = Convert.ToInt32(svar);
+
+                }
+            } catch
+            {
+                Console.WriteLine("Du kan endast skriva ett heltal eller 'n'");
+            }
+            
+            
+        }
+
+        Console.WriteLine(störstHeltal);
+        Console.ReadKey();
+    }
+}
